bcce469eeb2c477fbe8f802b1f50af40dce5aed4
Vito De Tullio <vito.detullio@gmail.com>
2021-04-22 11:33:22 +0200

    eagerly read config from local file

--- src/configurations/configurations.js
+++ src/configurations/configurations.js
@@ -1,10 +1,19 @@
 const {hostname} = require('os');
+const {load} = require('yaml-js');
+const {readFileSync} = require('fs');
 
 const EC2_HOSTNAME = 'ip-172-31-36-240';
 
-function running_on_ec2() {
-    return hostname() === EC2_HOSTNAME;
-}
+const {PRIVKEY_PATH, CERT_PATH} = require(hostname() === EC2_HOSTNAME
+    ? './aws'
+    : './local');
 
-if (running_on_ec2()) module.exports = require('./aws');
-else module.exports = require('./local');
+const CONFIG_PATH = process.env['CONFIG_URL'] || 'config.yml';
+
+const CREDENTIALS = {
+    key: readFileSync(PRIVKEY_PATH, 'utf8'),
+    cert: readFileSync(CERT_PATH, 'utf8')
+};
+
+module.exports.CONFIG = load(readFileSync(CONFIG_PATH, 'utf-8'));
+module.exports.CREDENTIALS = CREDENTIALS;
