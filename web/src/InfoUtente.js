45bcc3fe866ec2116388b4aafa64390b171f5449
vismarac <cinzia.vismara@finconsgroup.com>
2021-05-13 22:22:38 +0200

    format

--- web/src/InfoUtente.js
+++ web/src/InfoUtente.js
@@ -3,47 +3,47 @@
 import { get_username } from './cognitoclient.js';
 
 class InfoUtente extends React.Component {
-    render() {
-      return (
-        <div>
-          <UseraName />
-          <div className="grid-container grid-container__nested">
-              <div className="hidden-xs hidden-sm grid-cl-2-desktop">
-                  <div>
-                      <img src="" alt="user-icon"/>
-                  </div>
-              </div>
-              <div className="grid-cl-2-mobile grid-cl-6-tablet grid-cl-7-desktop">
-                  <div className="statistiche">
-                      <p>Vittorie: 0</p>
-                      <p>Sconfitte: 0</p>
-                      <p>Pareggi: 0</p>
-                  </div>
-              </div>
-              <div className="grid-cl-2-mobile grid-cl-6-tablet grid-cl-3-desktop statistiche__container-link">
-                  <a href="#">Storico partite</a>
-              </div>
-          </div>
+  render() {
+    return (
+      <div>
+        <UseraName />
+        <div className="grid-container grid-container__nested">
+            <div className="hidden-xs hidden-sm grid-cl-2-desktop">
+                <div>
+                    <img src="" alt="user-icon"/>
+                </div>
+            </div>
+            <div className="grid-cl-2-mobile grid-cl-6-tablet grid-cl-7-desktop">
+                <div className="statistiche">
+                    <p>Vittorie: 0</p>
+                    <p>Sconfitte: 0</p>
+                    <p>Pareggi: 0</p>
+                </div>
+            </div>
+            <div className="grid-cl-2-mobile grid-cl-6-tablet grid-cl-3-desktop statistiche__container-link">
+                <a href="#">Storico partite</a>
+            </div>
         </div>
-      )
-    }
+      </div>
+    )
+  }
+}
+
+class UseraName extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: null};
   }
 
-  class UseraName extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {name: null};
-    }
-
-    componentDidMount() {
-      this.setState({name: get_username()});
-    }
-
-    render() {
-      return (
-        <h2 className="title">{this.state.name}</h2>
-      )
-    }
+  componentDidMount() {
+    this.setState({name: get_username()});
   }
+
+  render() {
+    return (
+      <h2 className="title">{this.state.name}</h2>
+    )
+  }
+}
 
 export default InfoUtente;
