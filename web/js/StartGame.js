194c8885db139149d7be45512fc6d1602b2b5e1e
vismarac <cinzia.vismara@finconsgroup.com>
2021-05-15 20:35:49 +0200

    componente start game

--- web/js/StartGame.js
+++ web/js/StartGame.js
@@ -8,6 +8,10 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var colorOptions = [{ value: 'white', label: 'Bianco' }, { value: 'black', label: 'Nero' }];
+
+var opponentOptions = [{ value: 'cpu', label: 'CPU' }, { value: 'friend', label: 'Amico' }];
+
 var StartGame = function (_React$Component) {
     _inherits(StartGame, _React$Component);
 
@@ -25,17 +29,80 @@ var StartGame = function (_React$Component) {
         value: function render() {
             return React.createElement(
                 'div',
-                null,
-                React.createElement(
-                    'h2',
-                    null,
-                    'This is ComponentTwo'
-                )
+                { className: 'form__group' },
+                React.createElement(FormGroupElements, { name: 'color', id: 'colori', options: colorOptions, label: 'Colore', htmlFor: 'colors' }),
+                React.createElement(FormGroupElements, { name: 'opponent', id: 'opponent', options: opponentOptions, label: 'Sfidante', htmlFor: 'opponent' })
             );
         }
     }]);
 
     return StartGame;
+}(React.Component);
+
+var FormGroupElements = function (_React$Component2) {
+    _inherits(FormGroupElements, _React$Component2);
+
+    function FormGroupElements() {
+        _classCallCheck(this, FormGroupElements);
+
+        return _possibleConstructorReturn(this, (FormGroupElements.__proto__ || Object.getPrototypeOf(FormGroupElements)).apply(this, arguments));
+    }
+
+    _createClass(FormGroupElements, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { className: 'form__group__wrapper' },
+                React.createElement(Select, { name: this.props.name, id: this.props.id, options: this.props.options }),
+                React.createElement(
+                    'label',
+                    { htmlFor: this.props.htmlFor, className: 'form-group__label' },
+                    this.props.label
+                )
+            );
+        }
+    }]);
+
+    return FormGroupElements;
+}(React.Component);
+
+var Select = function (_React$Component3) {
+    _inherits(Select, _React$Component3);
+
+    function Select() {
+        _classCallCheck(this, Select);
+
+        var _this3 = _possibleConstructorReturn(this, (Select.__proto__ || Object.getPrototypeOf(Select)).call(this));
+
+        _this3.onChange = _this3.onChange.bind(_this3);
+        return _this3;
+    }
+
+    _createClass(Select, [{
+        key: 'onChange',
+        value: function onChange(event) {
+            console.log(event.target.value);
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var options = this.props.options;
+            return React.createElement(
+                'select',
+                { id: this.props.id, name: this.props.name, onChange: this.onChange },
+                options.map(function (item, index) {
+                    return React.createElement(
+                        'option',
+                        { value: item.value, key: index },
+                        item.label
+                    );
+                })
+            );
+        }
+    }]);
+
+    return Select;
 }(React.Component);
 
 export default StartGame;
--- web/src/StartGame.js
+++ web/src/StartGame.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const colorOptions = [
+    { value: 'white', label: 'Bianco' },
+    { value: 'black', label: 'Nero' }
+]
+
+const opponentOptions = [
+    { value: 'cpu', label: 'CPU' },
+    { value: 'friend', label: 'Amico' }
+]
+
 class StartGame extends React.Component{
     constructor(props) {
         super(props);
@@ -7,10 +17,45 @@ class StartGame extends React.Component{
     }
     render(){
         return (
-        <div>
-            <h2>This is ComponentTwo</h2>
-            
-        </div>)
+            <div className="form__group">
+                <FormGroupElements name="color" id="colori" options={colorOptions} label="Colore" htmlFor="colors"/>
+                <FormGroupElements name="opponent" id="opponent" options={opponentOptions} label="Sfidante" htmlFor="opponent"/>
+            </div>
+        )
+    }
+}
+
+class FormGroupElements extends React.Component{
+    render(){
+        return (
+            <div className="form__group__wrapper">
+                <Select name={this.props.name} id={this.props.id} options={this.props.options}/>
+                <label htmlFor={this.props.htmlFor} className="form-group__label">{this.props.label}</label>
+            </div>
+        )
+    }
+}
+
+class Select extends React.Component{
+    
+    constructor() {
+        super();
+        this.onChange = this.onChange.bind(this);
+    }
+    
+    onChange(event) {
+        console.log(event.target.value);
+    }
+
+    render(){
+        const options = this.props.options;
+        return(
+            <select id={this.props.id} name={this.props.name} onChange={this.onChange}>
+                {options.map((item, index) => {
+                  return <option value={item.value} key={index}>{item.label}</option>;
+                })}
+            </select>
+        )
     }
 }
 
